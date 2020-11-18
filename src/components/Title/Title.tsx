c3f83d483eb84cc321a9f7e7dcfb55d4f0fc2741
Andrew Yan <zushenyan@gmail.com>
2020-11-18 23:58:09 +0800

    upgrade CRA to the latest

--- src/components/Title/Title.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-interface TitleProps {
-  textColor?: string;
-}
-
-export default styled.h1`
-  color: ${({ textColor = 'red' }: TitleProps): string => textColor};
-`;
---
+++ src/foo/Counter.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function Counter () {
+  return null
+}
+
+export default Counter
